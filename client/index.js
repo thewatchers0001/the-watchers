19907105a79edc3295b8f7eba02168364e3e4f92
Kevin Weber <kevinweber@users.noreply.github.com>
2022-08-27 13:48:15 +0200

    Fix typo

--- client/index.js
+++ client/index.js
@@ -11,7 +11,7 @@ function App() {
           <p><img src="./media/logo-a1-fff.png" alt="The Watchers logo" width="110" height="51" /></p>
           <p>We are a community-driven collab committee for holders of Admit One NFTs by gmoney.</p>
           <p>Follow us on Twitter: <a href="https://twitter.com/thewatchers0001" title="The Watchers on Twitter">@TheWatchers0001</a></p>
-          <p>We run a DAO with the goal to strenghten the Admit One community.</p>
+          <p>We run a DAO with the goal to strengthen the Admit One community.</p>
           <p>Our collection: <a href="https://www.prysm.xyz/the-watchers" title="The Watchers Prysm Squad">prysm.xyz/the-watchers</a></p>
           <p class="handwriting">With love,<br/>The Watchers</p>
         </div>
