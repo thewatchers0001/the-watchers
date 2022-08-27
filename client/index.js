8c787d5ad73239bdc7c7a1371ff926971bf27aa6
Kevin Weber <kevinweber@users.noreply.github.com>
2022-08-27 17:34:32 +0200

    Update copy

--- client/index.js
+++ client/index.js
@@ -11,8 +11,8 @@ function App() {
           <p><img src="./media/logo-a1-fff.png" alt="The Watchers logo" width="110" height="51" /></p>
           <p>We are a community-driven collab committee for holders of Admit One NFTs by gmoney.</p>
           <p>Follow us on Twitter: <a href="https://twitter.com/thewatchers0001" title="The Watchers on Twitter">@TheWatchers0001</a></p>
-          <p>We run a DAO with the goal to strengthen the Admit One community.</p>
-          <p>Our collection: <a href="https://www.prysm.xyz/the-watchers" title="The Watchers Prysm Squad">prysm.xyz/the-watchers</a></p>
+          <p>Our multisig wallet is signed by all nine committee members and requires 5 of 9 signatures to complete on-chain transactions.</p>
+          <p>We welcome NFT and ETH donations: <a href="https://www.prysm.xyz/the-watchers" title="The Watchers Prysm Squad">prysm.xyz/the-watchers</a></p>
           <p class="handwriting">With love,<br/>The Watchers</p>
         </div>
         <div class="col col-50 highlight-background"></div>
