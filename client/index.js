9f01a808b0697d3afc25de3ecd123468a1f20445
Kevin Weber <kevinweber@users.noreply.github.com>
2023-06-08 11:03:37 +0200

    Change Prysm to Lore.xyz

--- client/index.js
+++ client/index.js
@@ -12,7 +12,7 @@ function App() {
           <p>We are a community-driven collab committee for holders of Admit One NFTs by Gmoney.</p>
           <p>Follow us on Twitter: <a href="https://twitter.com/thewatchers0001" title="The Watchers on Twitter">@TheWatchers0001</a></p>
           <p>Our multisig wallet is signed by all committee members and requires a majority vote to complete on-chain transactions.</p>
-          <p>We welcome NFT and ETH donations: <a href="https://www.prysm.xyz/the-watchers" title="The Watchers Prysm Squad">prysm.xyz/the-watchers</a></p>
+          <p>We welcome NFT and ETH donations: <a href="https://lore.xyz/p/the-watchers" title="The Watchers Lore Collective">lore.xyz/p/the-watchers</a></p>
           <p class="handwriting">With love,<br/>The Watchers</p>
         </div>
         <div class="col col-50 highlight-background"></div>
